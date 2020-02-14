9cd0a70b3e951caabf98f1e3aa20b8a363691790
Ricardo Ruiz <ricardo.ruiz@globant.com>
2020-02-13 18:32:01 -0600

    Adding Basic Spring boot project created with spring io.

---
+++ src/main/java/com/globant/brainwaves/BwSegmenterApplication.java
@@ -0,0 +1,13 @@
+package com.globant.brainwaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwSegmenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwSegmenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globant/brainwaves/BwSegmenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.brainwaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwSegmenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
